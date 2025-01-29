35a0e45569b228e757edc0820944576af9c95861
Franco Aimaro <aimarofran@gmail.com>
2025-01-29 10:10:55 -0300

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,7 +9,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
-import { Merchant } from './collections/Merchant'
+import { Merchants } from './collections/Merchants'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -21,7 +21,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media,Merchant],
+  collections: [Users, Media,Merchants],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
