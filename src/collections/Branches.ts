7977c3ef5e6240f606690f22fcf23ae841bbe02b
Franco Aimaro <aimarofran@gmail.com>
2025-01-29 10:49:46 -0300

    Create Branches.ts
    agrego modelo de Branches

---
+++ src/collections/Branches.ts
@@ -0,0 +1,37 @@
+import type { CollectionConfig } from 'payload';
+
+export const Branches: CollectionConfig = {
+  slug: 'branches',
+  admin: {
+    useAsTitle: 'alias', // Usamos el alias como título en el admin
+  },
+  fields: [
+    {
+      name: 'alias',
+      type: 'text',
+      label: 'Alias',
+      required: true, // Campo obligatorio
+    },
+    {
+      name: 'address',
+      type: 'textarea',
+      label: 'Dirección',
+      required: true, // Campo obligatorio
+    },
+    {
+      name: 'isActive',
+      type: 'checkbox',
+      label: '¿Está Activa?',
+      defaultValue: true, // Por defecto está activa
+    },
+    {
+      name: 'merchant',
+      type: 'relationship',
+      label: 'Merchant',
+      relationTo: 'merchants', // Slug de la colección de comerciantes
+      required: true, // Relación obligatoria
+    },
+  ],
+};
+
+export default Branches;
